6523c9ca3c186c4e2b649bbb5dca46990c5a95c3
Mycode8253 <ponnamanda1729@gmail.com>
2020-03-05 22:36:28 +0530

    > Added  optimisations to learning.py file > That includes improved way of taking the voice > Well defined conditioning of answers given > Improved test flow (no sudden jerks) changed the initRecognition and instructionThreadFunction and added few widgets

--- learningtest.py
+++ learningtest.py
@@ -18,8 +18,10 @@ class LearningTest(tk.Frame):
         #master.geometry("500x200")                  # change geometry height and widht(height x width)
         self.progress_bar_determinate = ttk.Progressbar(self,orient=tk.HORIZONTAL,length=100, mode='determinate')
         self.progress_bar_indeterminate = ttk.Progressbar(self,orient=tk.HORIZONTAL,length=100, mode='indeterminate')
-        self.start_button = tk.Button(self,text="Start the Test",command= lambda: self.startThread())
+        self.start_button = tk.Button(self,text="Start the Test",command= self.startThread)
+        self.process_running_label = tk.Label(self,text="",font=('Calibre',20))
         self.start  = False
+        self.complete_flag = True
         self.createWindow()
         threading.Thread(target=self.instructionThread,args=(),daemon=True).start()
 
@@ -29,7 +31,7 @@ class LearningTest(tk.Frame):
 
     def createRandomNumber(self):
       random_letter =  random.choice(self.letters)  # generates a random letter from the given list of letters
-      self.letter_label['text'] = random_letter
+     
       return random_letter
 
 
@@ -62,14 +64,20 @@ class LearningTest(tk.Frame):
 
 
     def timer(self):
+      bar_level=10
       print("timer is called")
-      self.barThread(30,False)
-      time.sleep(10)
-      self.barThread(60,False)
-      time.sleep(5)
-      self.barThread(90,False)
-      time.sleep(5)
-      self.barThread(100,False)
+      while(True):
+        #self.lock.acquire()
+        if self.complete_flag:
+          self.barThread(100,False)
+          self.complete_flag  =False
+         # self.lock.release()
+          break
+        else:
+          self.barThread(bar_level+10,False)
+          #self.lock.release()
+          time.sleep(5)  
+      
 
 
 
@@ -80,49 +88,36 @@ class LearningTest(tk.Frame):
     def instructionThread(self,*awrgs,**kwargs):
       tag = "instructionThread:"
       print(tag +" i am called ")
-     
-    
+
       self.instruction_label['text']="You will be given a set of letters \n you have to speak out louder\n letter will stay for a period of time and it will disappear you should tell it when it disappears\n we will start when you are ready :-)"
       self.start_button.grid(row=3,column=2,pady=2)
-
+      self.process_running_label.grid(row=4,column=2)
       while(True):
         if self.start:
           self.start_button.grid_forget()
-          self.progress_bar_determinate.grid(row=4,column=2,pady=2)
+          self.progress_bar_determinate.grid(row=5,column=2,pady=2)
           self.barThread(0,False)
-          self.timer()
           break
         
       self.instruction_label['text'] = "Here they come"
       count=0
       while(True):
         thread = threading.Thread(target=self.initRecognition,args=(self.createRandomNumber()),daemon=True)
-        if not thread.isAlive():
+        self.lock.acquire()
+        if  (not thread.isAlive() )and (self.complete_flag) :
+          self.complete_flag= False
+          self.lock.release()
           if count <10:
             count+=1
+            
             thread.start()
-            self.timer()  
           else:
             break
-        
-        
-      
-
-
-
-
-     
-
+        else:
+          self.lock.release()
+      self.process_running_label.grid_forget()
       self.instruction_label['text'] = "done with the test"
-        
-
-
-      
-
-
-
-
-
+    
       print(tag+"i am going down")
 
 
@@ -130,27 +125,39 @@ class LearningTest(tk.Frame):
 
 
     def initRecognition(self,word,*args,**kwargs):
+        self.letter_label['text'] = word
         r=sr.Recognizer()
         mic= sr.Microphone()
         print("speak")
+        self.barThread(20,False)
+        self.process_running_label['text'] = "Speak I am Listening...."
         with mic as source:
-            try:
-                r.adjust_for_ambient_noise(source,duration=  1)
-                audio = r.listen(source, phrase_time_limit=10)
+            try:              
+                r.adjust_for_ambient_noise(source,duration=  0.1)
+                audio = r.listen(source, timeout=20,)
+                self.barThread(50,False)
                 
             except sr.WaitTimeoutError:
                 messagebox.showinfo('Message',"You didnt speak anything please speak again")
-        
-                
         try:
-            recognisedPhrase= r.recognize_google(audio)
-            print(recognisedPhrase)
-            if recognisedPhrase == word.lower():
-                print(True)
-            else:
-                print(False)
+          self.process_running_label['text'] = "Processing....Be patient"
+          recognisedPhrase= r.recognize_google(audio)
+          print(recognisedPhrase)
+          if recognisedPhrase == word.lower():
+              print(True)
+          else:
+              print(False)
+          
         except sr.UnknownValueError:
             messagebox.showinfo('Message',"What you spoke didnt make any sense try one more time ")
+        finally:
+          self.barThread(80,False)
+          self.process_running_label['text'] = "Almost Done !"
+          self.lock.acquire()
+          self.complete_flag = True
+          self.lock.release()
+          self.barThread(100,False)
+        
 
       
       
